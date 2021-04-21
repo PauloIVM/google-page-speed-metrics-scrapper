9470189c044c14e8b48318a52766accd558739e0
Paulo Marinho <pakas_vieira_95@hotmail.com>
2021-04-21 17:17:09 -0300

    fix(popup.js): fix duplicated functions

--- popup.js
+++ popup.js
@@ -3,30 +3,36 @@ let desktopMetricsButton = document.getElementById("desktop");
 
 mobileMetricsButton.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
+    chrome.storage.sync.set({deviceType: "mobile"});
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: showMobileMetrics,
+        function: copyLabMetrics,
     });
 });
 
 desktopMetricsButton.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
+    chrome.storage.sync.set({deviceType: "desktop"});
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: showDesktopMetrics,
+        function: copyLabMetrics,
     });
 });
 
-const showMobileMetrics = function() {
+const copyLabMetrics = function() {
     function getMetrics(elementBase) {
-        const fcp = elementBase.getElementsByClassName("lh-metric__value")[8].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
-        const si  = elementBase.getElementsByClassName("lh-metric__value")[9].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
-        const lcp = elementBase.getElementsByClassName("lh-metric__value")[10].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
-        const tti = elementBase.getElementsByClassName("lh-metric__value")[11].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
-        const tbt = elementBase.getElementsByClassName("lh-metric__value")[12].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
-        const cls = elementBase.getElementsByClassName("lh-metric__value")[13].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
+        const element = elementBase.getElementsByClassName("lh-metric__value");
+
+        // There is a problem here. If the element.length was different than 14,
+        // the scrapper will crash. This is because sometimes google displays a page
+        // with a different layout, but I was unable to reproduce that page
+
+        const fcp = element[8].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
+        const si  = element[9].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
+        const lcp = element[10].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
+        const tti = element[11].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
+        const tbt = element[12].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
+        const cls = element[13].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
         const page_speed = elementBase.getElementsByClassName("lh-gauge__percentage")[0].textContent;
         return (fcp+" "+si+" "+lcp+" "+tti+" "+tbt+" "+cls+" "+page_speed);
     }
@@ -40,52 +46,16 @@ const showMobileMetrics = function() {
         textArea.focus();
         textArea.select(); 
         try {
-          var successful = document.execCommand('copy');
-          var msg = successful ? 'successful' : 'unsuccessful';
-          console.log('Fallback: Copying text command was ' + msg);
+            document.execCommand('copy');
         } catch (err) {
-          console.error('Fallback: Oops, unable to copy', err);
+            console.error('Fallback: Oops, unable to copy', err);
         }  
         document.body.removeChild(textArea);
     }
-
-    const elementBase = document.getElementsByClassName("goog-control result lh-vars lh-root");
-    console.log("Mobile:");
-    console.log(getMetrics(elementBase[0]));
-    copyTextToClipboard(getMetrics(elementBase[0]))
-}
-
-const showDesktopMetrics = function() {
-    function getMetrics(elementBase) {
-        const fcp = elementBase.getElementsByClassName("lh-metric__value")[8].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
-        const si  = elementBase.getElementsByClassName("lh-metric__value")[9].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
-        const lcp = elementBase.getElementsByClassName("lh-metric__value")[10].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
-        const tti = elementBase.getElementsByClassName("lh-metric__value")[11].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
-        const tbt = elementBase.getElementsByClassName("lh-metric__value")[12].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
-        const cls = elementBase.getElementsByClassName("lh-metric__value")[13].textContent.match(/([0-9]*,[0-9]*)|([0-9]*)/i)[0];
-        const page_speed = elementBase.getElementsByClassName("lh-gauge__percentage")[0].textContent;
-        return (fcp+" "+si+" "+lcp+" "+tti+" "+tbt+" "+cls+" "+page_speed);
-    }
-    function copyTextToClipboard(text) {
-        var textArea = document.createElement("textarea");
-        textArea.value = text;
-        textArea.style.top = "0";
-        textArea.style.left = "0";
-        textArea.style.position = "fixed";     
-        document.body.appendChild(textArea);
-        textArea.focus();
-        textArea.select(); 
-        try {
-          var successful = document.execCommand('copy');
-          var msg = successful ? 'successful' : 'unsuccessful';
-          console.log('Fallback: Copying text command was ' + msg);
-        } catch (err) {
-          console.error('Fallback: Oops, unable to copy', err);
-        }  
-        document.body.removeChild(textArea);
-    }
-    const elementBase = document.getElementsByClassName("goog-control result lh-vars lh-root");
-    console.log("Destop:");
-    console.log(getMetrics(elementBase[1]));
-    copyTextToClipboard(getMetrics(elementBase[1]))
+    chrome.storage.sync.get(['deviceType'], function(result) {
+        const deviceType = result.deviceType;
+        const elementBase = document.getElementsByClassName("goog-control result lh-vars lh-root");
+        const index = deviceType === "mobile" ? 0 : 1;
+        copyTextToClipboard(getMetrics(elementBase[index]))
+    });
 }
